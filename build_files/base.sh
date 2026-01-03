ccf43ad83539c8b01feecd6a7a2eaa2ebfd21fb8
ExistingPerson <164225044+ExistingPerson08@users.noreply.github.com>
2026-01-03 15:33:10 +0000

    Remove groupadd

--- build_files/base.sh
+++ build_files/base.sh
@@ -44,7 +44,6 @@ systemctl enable polkit.service \
     cups-browsed.service
 
 # Users
-groupadd -r avahi
 useradd -r -g avahi -d /var/run/avahi-daemon -s /usr/bin/nologin -c "Avahi mDNS/DNS-SD Stack" avahi
 
 # Cleanup
