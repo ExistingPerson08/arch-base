bbe5070e81158c01a69a30d744de8b2d05fe2625
ExistingPerson <dedek.vlastimil2@proton.me>
2026-01-03 13:47:28 +0100

    remove 32bit drivers

--- build_files/base.sh
+++ build_files/base.sh
@@ -31,8 +31,8 @@ pacman -Sy --noconfirm --needed \
     cosmic cosmic-session cosmic-greeter
 
 # Drivers
-pacman -S --noconfirm amd-ucode intel-ucode efibootmgr shim mesa lib32-mesa libva-intel-driver libva-mesa-driver \
-    vpl-gpu-rt vulkan-icd-loader vulkan-intel vulkan-radeon apparmor xf86-video-amdgpu lib32-vulkan-radeon zram-generator \
+pacman -S --noconfirm amd-ucode intel-ucode efibootmgr shim mesa libva-intel-driver libva-mesa-driver \
+    vpl-gpu-rt vulkan-icd-loader vulkan-intel vulkan-radeon apparmor xf86-video-amdgpu zram-generator \
     lm_sensors intel-media-driver
 
 # Initramfs
